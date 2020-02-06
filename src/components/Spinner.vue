34dc80a875245a3a35ce7d6729930793b8327c50
ilbu <ilbu@solvve.com>
2020-02-07 01:45:36 +0200

    Create spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="spinner" :style="{ '--size': `${size}px` }"></div>
+</template>
+
+<script>
+export default {
+  name: 'Spinner',
+  props: {
+    size: {
+      type: Number,
+      default: 30, // px
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../assets/scss/colors.scss';
+@import '../assets/scss/animations.scss';
+
+.spinner {
+  width: var(--size);
+  height: var(--size);
+  border: solid 2px transparent;
+  border-left-color: $primary;
+  border-bottom-color: $primary;
+  border-radius: 50%;
+  animation: spin 0.5s linear infinite;
+  filter: blur(0.5px);
+}
+</style>
